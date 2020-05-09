9a411f2e528bcdedd48b4221dbd304f1270cb215
Arzu <arzuozkan1905@gmail.com>
2020-05-10 01:42:12 +0300

    intersect methodin group class is created for group sphere intersection

---
+++ assignment1/scene2.py
@@ -0,0 +1,165 @@
+# CENG 415 Applications of Computer Graphics
+# Student: Arzu ÖZKAN 16050111051
+# Assignment 1
+
+import json
+import numpy as np
+import math
+from PIL import Image, ImageDraw
+
+
+class Object3D:
+    def __init__(self, color):
+        self.color = color
+
+    def intersect(ray, hit=0, tmin=0):
+        pass
+
+
+class Sphere(Object3D):
+    def __init__(self, data, ind=0):
+        self.radiusSphere = data['group'][ind]['sphere']['radius']
+        self.centerSphere = data['group'][ind]['sphere']['center']
+        self.color = tuple(np.array(data['group'][ind]['sphere']['color']))
+
+    def intersect(self, ray, hit=None, tmin=0.001):
+        oc = ray.origin - self.centerSphere
+        a = np.dot(ray.direction, ray.direction)
+        b = 2 * np.dot(oc, ray.direction)
+        c = np.dot(oc, oc) - pow(self.radiusSphere, 2)
+        d = (b * b - 4 * a * c)
+        if d < 0:
+            return -1
+        else:
+            d = math.sqrt(d)
+            t1 = (-b + d) / (2 * a)
+            t2 = (-b - d) / (2 * a)
+            if tmin < t1 <= t2:
+                t = t1
+            t = t2
+            hit.t = t
+            hit.color = self.color
+            return t
+
+
+class Group(Object3D):
+
+    def __init__(self):
+        self.objects = []
+
+    def addSphere(self, sphere):
+        self.objects.append(sphere)
+
+    def intersect(self, ray, hit, tmin=0):
+        hit_tmp = []
+        for s in self.objects:
+            hit_tmp.append(s.intersect(ray, hit))
+        print("hit_tmp: ", hit_tmp)
+        print(np.mean(hit_tmp))
+        if np.mean(hit_tmp) != -1:
+            min_t = min(hit_tmp)
+            print("min_t: ", min_t)
+            hit.t = min_t
+            hit.color = self.objects[hit_tmp.index(min_t)].color
+            return min_t
+        else:
+            hit.color = back_color
+            return -1
+
+
+class Camera:
+    def generateRay(x, y):
+        pass
+
+
+class OrthographicCamera(Camera):
+    def __init__(self, data):
+        self.center = np.array(data['orthocamera']['center'])
+        self.dir = np.array(data['orthocamera']['direction']) * -1
+        self.up = np.array(data['orthocamera']['up'])
+        self.size = data['orthocamera']['size']
+
+    def generateRay(self, x, y):
+        horizontal = np.cross(self.dir, self.up)
+        return (self.center + (x - 0.5) * self.size * horizontal + (y - 0.5) * self.size * self.up)
+
+
+class Ray:
+    def __init__(self, origin, direction):
+        self.origin = origin
+        self.direction = direction
+
+
+class Hit:
+    def __init__(self, t=0, color=(0, 0, 0)):
+        self.t = t
+        self.color = color
+
+
+# normalization
+def findNormalize(x, y, size):
+    return ((x + 0.5) / size[0], (y + 0.5) / size[1])
+
+
+def rayTracingColor(image, hit, g):
+    pixel = image.load()
+    # ray tracing for every pixel
+    for i in range(SIZE[0]):
+        for j in range(SIZE[1]):
+            x, y = findNormalize(i, j, SIZE)
+            r = orthcam.generateRay(x, y)
+            ray = Ray(r, orthcam.dir)
+            g.intersect(ray, hit)
+            # make control the t whether there is a hit
+            if h.t != -1:
+                pixel[i, j] = tuple(hit.color)
+            else:
+                pixel[i, j] = tuple(back_color)
+    image.show()
+
+
+def rayTracingDepth(image, hit, g):
+    # pixels
+    pixel = image.load()
+    # ray tracing for every pixel
+    near = 9
+    far = 11
+    for i in range(SIZE[0]):
+        for j in range(SIZE[1]):
+            x, y = findNormalize(i, j, SIZE)
+            r = orthcam.generateRay(x, y)
+            ray = Ray(r, orthcam.dir)
+            t = g.intersect(ray, hit)
+            # make control the t whether there is a hit
+            if t != -1:
+                depth = int(round((far - t) / (far - near) * 255) - 1)
+                hit.color = (depth, depth, depth)
+                pixel[i, SIZE[0] - j - 1] = hit.color
+            else:
+                pixel[i, SIZE[0] - j - 1] = tuple(back_color)
+    image.show()
+
+
+def makeGroup(group, data):
+    for i in range(5):
+        group.addSphere(Sphere(data, i))
+
+
+if __name__ == '__main__':
+    # read the json file
+    with open('scene2.json') as f:
+        data = json.load(f)
+
+    SIZE = (250, 250)
+    back_color = np.array(data['background']['color'])
+    # objects
+    orthcam = OrthographicCamera(data)
+    groupSphere = Group()
+    h = Hit()
+    im = Image.new('RGB', SIZE, tuple(back_color))
+
+    makeGroup(groupSphere, data)
+
+    rayTracingColor(im, h, groupSphere)
+    # rayTracingDepth(im, h)
+    print("OK")
